fbcbb78bf3949e374834a902c0f4d8bdd8d1a3b7
Dai Kawahara <skbsporta7@gmail.com>
2020-05-10 20:05:59 +0900

    Initial commit

---
+++ Variable-TextView-Keyboard-Accessory-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Variable-TextView-Keyboard-Accessory-Sample
+//
+//  Created by kawaharadai on 2020/05/10.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
